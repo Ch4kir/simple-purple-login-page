6654664e4eb83ffaffb7d5363ea454ee3af7e2cb
fri chakir <chakir.fri@gmail.com>
2023-01-15 12:31:01 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import Form from './Form';
+import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme =()=>{
+    setTheme((curr)=>(curr === "light" ? "dark" :"light"))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeContext.Provider value={{theme, toggleTheme}} >
+    <div className="App" id={theme}>
+      <Form/>
+      <div className="switch">
+        <label> {theme === "light" ? "Light mode" : "Dark mode"} </label>
+      <ReactSwitch 
+      onColor="#38004c"
+      onHandleColor="#600780"
+      handleDiameter={30}
+      uncheckedIcon={false}
+      checkedIcon={false}
+      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+      activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+      height={20}
+      width={48}
+      className="react-switch"
+      id="material-switch"
+      onChange={toggleTheme} 
+      checked={theme==="dark"}
+      />
+      </div>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
